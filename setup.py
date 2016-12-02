45eee75706625f8e0e03be0216a0063cae59577c
David Marquis <davidmarquis@me.com>
2016-12-02 18:20:54 -0500

    Adding missing modules and bumping version

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from distutils.core import setup
 setup(
     name = 'pyperiods',
-    packages = ['pyperiods'],
-    version = '0.1',
+    packages = [
+        'pyperiods',
+        'pyperiods.django',
+        'pyperiods.restframework',
+    ],
+    version = '0.9',
     description = 'Tools for representing and manipulating periods of time (i.e. months and years)',
     author = 'David Marquis',
     author_email = 'david@radiant3.ca',
     url = 'https://github.com/davidmarquis/pyperiods',
-    download_url = 'https://github.com/davidmarquis/pyperiods/tarball/0.1',
+    download_url = 'https://github.com/davidmarquis/pyperiods/tarball/0.9',
     keywords = ['period', 'year', 'years', 'month', 'months'],
     classifiers = [],
 )
