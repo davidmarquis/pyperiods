93c2e4823a1715554f8076bb39b20572f8cf8558
David Marquis <davidmarquis@me.com>
2016-12-07 14:55:24 -0500

    Release script manages tags

--- release.sh
+++ release.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
+VERSION=$1
+
+if [ -z "$VERSION" ]; then
+    echo "Usage: ./release.sh [VERSION]"
+fi
+
 source env/bin/activate
 python -m unittest discover
+
+git tag $VERSION
+tag push --tags origin
 
 python setup.py sdist upload -r pypi
